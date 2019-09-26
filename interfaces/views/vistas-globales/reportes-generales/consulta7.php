264256f213002b4ca78cf940daa3c6a379d9a478
fray <fdmontoya0@misena.edu.co>
2019-09-26 07:24:22 -0500

    edicion en vistas globales, posibles consultas

--- interfaces/views/vistas-globales/reportes-generales/consulta7.php
+++ interfaces/views/vistas-globales/reportes-generales/consulta7.php
@@ -1,3 +1,15 @@
 <div class="style-float">
 Aprendices Citados Por Ficha Y Program
 </div>
+
+<?php 
+$query="SELECT af.numFicha,p.nombre,r.fecha,r.consecutivo,af.docIDAprendiz,u.nombres
+ FROM tblreporte AS r INNER JOIN tblaprendicesreportados AS ar ON ar.consReporte=r.consecutivo
+  INNER JOIN tblaprendicesficha as af ON af.docIDAprendiz=ar.docIDAprendiz 
+  INNER JOIN tblficha as f ON f.nroFicha=af.numFicha
+   INNER JOIN tblprograma as p ON p.codigo=f.programa
+    inner join tblusuario as u on af.docIDAprendiz=u.docID 
+    order by f.nroFicha";
+
+
+?>
